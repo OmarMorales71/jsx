3b4f14c8dda2d695b3e24b3457af1a9b5e482b75
Omar Morales Ortega <morales.ortega.omar@gmail.com>
2020-06-09 18:17:00 -0500

    TopBar Added

---
+++ src/TopBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TopBar = ()=>(
+    <header className="main-header">
+  <div className="topbar text-color">
+    <div className="ed-grid m-grid-2">
+      <p className="s-mb-1 m-mb-0 s-center m-left">Proyecto React</p>
+      <a className="button--cta button--tiny s-to-center m-to-right" href="https://www.youtube.com">Youtube</a>
+    </div>
+  </div>
+  </header>
+)
+
+export default TopBar;
