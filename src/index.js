d8a904fd2a31b5a149452ee539142d8b8e679c43
Omar Morales Ortega <morales.ortega.omar@gmail.com>
2020-06-08 20:32:46 -0500

    Primer Commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const root = document.getElementById('root')
+
+//const elemento = React.createElement(componente, propiedades, hijos)
+//const elemento = React.createElement("h1", {className: "saludo"}, "Hola Mundo")
+
+
+
+ReactDOM.render(<App />, root);
+
